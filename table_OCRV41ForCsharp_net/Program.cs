c37ab8c1d631399fa33b8dd138fc69a50cee2b3a
te646445026 <646445026@qq.com>
2024-07-12 19:00:48 +0800

    //

--- table_OCRV41ForCsharp_net/Program.cs
+++ table_OCRV41ForCsharp_net/Program.cs
@@ -627,6 +627,7 @@ namespace table_OCRV41ForCsharp
             if (!File.Exists(path.DefaultJsonFilePath))
             {               
                 //选择默认路径
+
                 MessageBox.Show("请选择需要识别的图片所在文件夹");
                 FolderBrowserDialog folder1 = new FolderBrowserDialog();
                 folder1.Description = "请选择需要识别的图片所在文件夹";
