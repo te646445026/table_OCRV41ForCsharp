fa0bbabeeadfc61d22c26152b263c440aa2fc752
te646445026 <646445026@qq.com>
2025-08-08 20:50:44 +0800

    feat: 初始化.NET Framework 4.7.2项目结构和配置文件
    添加项目基础结构、配置文件和迁移文档

---
+++ table_OCRV41ForCsharp_net_framework/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace table_OCRV41ForCsharp_net_framework
+{
+    /// <summary>
+    /// 程序入口类
+    /// 注意：这是临时文件，将在第二阶段迁移时被实际代码替换
+    /// </summary>
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("table_OCRV41ForCsharp_net_framework 项目已创建");
+            Console.WriteLine("等待第二阶段代码迁移...");
+            Console.WriteLine("按任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ table_OCRV41ForCsharp_net_framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的元数据。
+[assembly: AssemblyTitle("table_OCRV41ForCsharp_net_framework")]
+[assembly: AssemblyDescription("腾讯云OCR表格识别应用程序 - .NET Framework 4.7.2 版本")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("table_OCRV41ForCsharp_net_framework")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("8b2a4f5c-9d3e-4a1b-8c7f-2e6d9a5b4c3e")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+//可以指定所有这些值，也可以使用"生成号"和"修订号"的默认值
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
